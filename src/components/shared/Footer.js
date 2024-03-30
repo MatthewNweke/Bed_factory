4b2c46a04d2cc528e772f3bf2e6321b187c8386b
MatthewNweke01~ <nwekematthew243@gmail.com>
2024-03-30 14:04:05 +0100

    footerCreated

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,16 +1,51 @@
+import Link from "next/link";
 
-const Footer = () =>
-{
-    return (
-        <footer className="bg-gray-800 p-6 text-center text-white mt-20">
-            <p className="mb-6">© { new Date().getFullYear() } Your Site. All rights reserved.</p>
-            <div className="flex justify-center space-x-4">
-                <a href="#" className="hover:underline">Terms</a>
-                <a href="#" className="hover:underline">Privacy</a>
-                <a href="#" className="hover:underline">Contact</a>
-            </div>
-        </footer>
-    );
+const Footer = () => {
+  return (
+    <footer className="bg-white p-6  text-black shadow-xl border-t-2 border-black-800">
+      <div className="flex justify-around py-6 space-x-4 p-0">
+        <ul>
+            quality materials good designs, <br/> professional craftsmanship and <br/> sustainability
+        </ul>
+        <ul className="p-0 leading-9">
+          <li className="font-semibold mb-5">Shops</li>
+          <li className="mb-2"><Link href="">Clothing</Link></li>
+          <li className="mb-2"><Link href="">Shoes</Link></li>
+          <li className="mb-2"><Link href="">Accessories</Link></li>
+          <li className="mb-2"><Link href="">Brands</Link></li>
+          <li className="mb-2"><Link href="">Sale</Link></li>
+          <li className="mb-2"><Link href="">Gift Cards</Link></li>
+        </ul>
+        <ul className="p-0 leading-9">
+          <li className="font-semibold mb-5">Gift Cards</li>
+          <li className="mb-2"><Link href="">Buy Gift Cards</Link></li>
+          <li className="mb-2"><Link href="">About Gift Cards</Link></li>
+          <li className="mb-2"><Link href="">Redeem a Gift Card</Link></li>
+          <li className="mb-2"><Link href="">Corporate Gift <br/> Cards</Link></li>
+          <li className="mb-2"><Link href="">Subscribe</Link></li>
+        </ul>
+
+        <ul className="p-0 ">
+          <li className="font-semibold mb-5">About store</li>
+          <li className="mb-2"><Link href="" >About Us</Link></li>
+          <li className="mb-2"><Link href="">Support</Link></li>
+          <li className="mb-2"><Link href="">Careers</Link></li>
+          <li className="mb-2"><Link href="">Newsroom</Link></li>
+          <li className="mb-2"><Link href="">Investors</Link></li>
+        </ul>
+
+        <ul className="p-0 leading-9">
+          <li className="font-semibold mb-5">Legal</li>
+          <li className="mb-2">Legal Notice</li>
+          <li className="mb-2">Privacy Policy</li>
+          <li className="mb-2">Terms and Conditions</li>
+        </ul>
+      </div>
+      <p className="mt-10 text-center">
+        © {new Date().getFullYear()} Synergy-IT. All rights reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -8,19 +8,7 @@ const Navbar = () => {
             <p className="cursor-pointer text-sm">English</p>
           </div>
         </div>
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-
+        
         <div className="text-white">
          Free delivery on orders above $50
         </div>
@@ -42,6 +30,7 @@ const Navbar = () => {
          
           </div>
         </div>
+        
       </nav>
     {/* <div className=""> */}
         <ul className="p-0 w-[100%] bg-white h-[10vh] shadow-xl flex items-center gap-5 px-20">
