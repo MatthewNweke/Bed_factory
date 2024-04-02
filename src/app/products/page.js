51f9e3aed74c58d3b32de57db5f4e9165a8076be
MatthewNweke01~ <nwekematthew243@gmail.com>
2024-04-02 17:35:28 +0100

    recent

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -12,250 +12,291 @@ const Products = () => {
   return (
     <div className="my-10 max-md:px-5">
       <div className="grid grid-cols-3 grid-rows-11 place-items-center items-center content-center gap-[2.75rem] max-gap-x-[1rem] max-md:grid-cols-2 max-md:grid-rows-16  max-sm:grid-cols-1 max-sm:grid-rows-33">
-        <div className="shadow-xl">
-        <Image className=""
+        <div className="shadow-xl max-sm:w-[100%] max-sm:w-[100%]">
+        <Image className="max-[400px] w-[100%]"
             src={bed_img2}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img3}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img4}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img5}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img6}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img7}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img8}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img1}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img2}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img3}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img4}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img5}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img5}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img6}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img7}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img7}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img8}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img1}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img2}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img2}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img3}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img4}
             width={250}
             height={250}
             alt="Bed Picture"
           />
           </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img5}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img6}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img7}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl">
+        <div className="shadow-xl max-sm:w-[100%]">
         <Image
+        className="max-[400px] w-[100%]"
             src={bed_img8}
             width={250}
             height={250}
             alt="Bed Picture"
           />
         </div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img1}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img2}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img3}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img4}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img5}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img6}
             width={250}
             height={250}
             alt="Bed Picture"
           /></div>
-        <div className="shadow-xl"><Image
+        <div className="shadow-xl max-sm:w-[100%]">
+          <Image
+          className="max-[400px] w-[100%]"
             src={bed_img6}
             width={250}
             height={250}
